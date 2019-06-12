c6238177f5f5f7287f4eb52eb31e0f5d25a2d916
PetejStone <petejstoney@gmail.com>
2019-06-12 15:18:03 -0700

    added login and users compoennts

---
+++ client/src/components/Login.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {login, signUp, logOut} from '../actions';
+
+
+class Login extends React.Component {
+  state = {
+    loginForm: true,
+    signupForm: false,
+    credentials: {
+        username: '',
+        password: ''
+    },
+    user: {
+        username: '',
+        password: ''
+        }
+}
+
+handleChange = e => {
+  console.log(e.target.value)
+  if (this.state.loginForm === true) {
+      console.log('LOGIN')
+  this.setState({
+      credentials: {
+           ...this.state.credentials,
+           [e.target.name] : e.target.value
+
+      }
+  })
+  } else  {
+      console.log('SIGNUP')
+      this.setState({
+          user: {
+              ...this.state.user,
+              [e.target.name] : e.target.value
+          }
+      })
+  }
+  console.log(this.state.user)
+}
+
+login = e => {
+  e.preventDefault();
+ console.log(this.state.credentials)
+ this.props.login(this.state.credentials).then(() => {
+ this.props.history.push('/users')
+});
+}
+
+  render() {
+  return (
+    <div className="App">
+      <form onSubmit={this.login}>
+        <label for="username">Username</label>
+        <input id="username" type="text" name="username" value={this.state.credentials.username} onChange={this.handleChange} required/>
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" value={this.state.credentials.password} onChange={this.handleChange} required/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+  }
+}
+
+const mapStateToProps = ({isLoggingIn, error, newUser, pending}) => ({
+  isLoggingIn,
+  error,
+  newUser,
+  pending
+
+});
+
+export default connect(mapStateToProps,{login, signUp, logOut})(Login)
---
+++ client/src/components/Users.js
@@ -0,0 +1,45 @@
+import React from 'react';
+//import {Route} from 'react-router-dom';
+
+import {getData} from '../actions'
+import {connect} from 'react-redux';
+
+class Users extends React.Component {
+  
+
+   componentDidMount() {
+       
+        this.props.getData()
+        
+      
+   }
+
+   logout = e => {
+    e.preventDefault();
+    localStorage.clear()
+    this.props.history.push('/')
+   
+
+   }
+    render() {
+        return (
+            <div>
+                <div>
+                {this.props.users.map(user =>
+                    <h1>{user.username}</h1>)}
+                </div>
+                <button onClick={this.logout}>Logout</button>
+            </div>
+          
+           
+        )
+    }
+    
+}
+
+const mapStateToProps = (state) => ({
+    credentials: state.credentials,
+    users: state.users
+})
+
+export default connect(mapStateToProps,{getData})(Users)
