0446edffbb48db4d4a232c2129e54609ef77668e
PetejStone <petejstoney@gmail.com>
2019-06-12 15:17:13 -0700

    added routes to index and app js, created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {Route} from 'react-router-dom';
+import Login from './components/Login';
+import Users from './components/Users';
+//import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/users" component={Users} />
     </div>
   );
 }
