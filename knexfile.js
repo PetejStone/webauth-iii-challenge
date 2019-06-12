0a39d9ba2580aa4ef98763a354c34bcd6ec31cad
PetejStone <petejstoney@gmail.com>
2019-06-12 12:38:53 -0700

    created database table through migration

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/database.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     }
   },
 
