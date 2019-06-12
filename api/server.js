084e710c80ceb43d0596c33580e4fdecbb10eadf
PetejStone <petejstoney@gmail.com>
2019-06-12 12:48:56 -0700

    created server folders and files and got server running

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+//const userRouter = ('./users/usersRouter')
+//const authRouter = ('./auth/authRouter')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+//server.use('/api/users', usersRouter)
+//server.use('/api/auth', authRouter)
+
+server.get('/', (req, res) => {
+    res.send('<h1> Server is Running!</h1>')
+})
+
+module.exports = server;
